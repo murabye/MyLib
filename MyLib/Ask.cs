f2937989990a4a0df76fee0146cccc3699fb1cf8
murabye <vlada_kuz2011@mail.ru>
2017-06-02 05:09:38 +0500

    переходы на строки

--- MyLib/Ask.cs
+++ MyLib/Ask.cs
@@ -6,28 +6,28 @@ namespace MyLib
     {
         public static int Num(string description, int min = int.MinValue, int max = int.MaxValue)
         {
-            Console.WriteLine(description);
+            Console.Write(description);
             int n;
             while (!(int.TryParse(Console.ReadLine(), out n)) || !(n >= min) || !(n <= max))
-                Console.WriteLine("Неверный ввод. ");
+                Console.Write("Неверный ввод. Повторите, пожалуйста: ");
 
             return n;
         }
         public static string Word(string description)
         {
-            Console.WriteLine(description);
+            Console.Write(description);
             string n;
             while (((n = Console.ReadLine()) == "") || n.Trim() == "") 
-                Console.WriteLine("Неверный ввод. ");
+                Console.WriteLine("Неверный ввод. Повторите, пожалуйста: ");
 
             return n;
         }
         public static double RealNum(string description, double min = double.MinValue, double max = double.MaxValue)
         {
-            Console.WriteLine(description);
+            Console.Write(description);
             double n;
             while (!(double.TryParse(Console.ReadLine(), out n)) || !(n >= min) || !(n <= max))
-                Console.WriteLine("Неверный ввод. ");
+                Console.Write("Неверный ввод. Повторите, пожалуйста: ");
 
             return n;
         }
